9e62b05a65104c557e0e2c94e62ee18cbbdb8d9f
dewshan.zincat <dewshan.zincat@gmail.com>
2021-10-25 23:49:00 +0530

    add

---
+++ pages/api/journey-detail.js
@@ -0,0 +1,14 @@
+import { connectToDatabase } from "../../util/mongodb";
+import Head from 'next/head'
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+
+    const journey_detail = await db
+    .collection("journey_detail")
+    .find({"key": req.body})
+    // .sort({ metacritic: -1 })
+    .limit(1)
+    .toArray();
+  res.json(journey_detail);
+};
---
+++ pages/api/visitor-count-by-click.js
@@ -0,0 +1,14 @@
+import { connectToDatabase } from "../../util/mongodb";
+import Head from 'next/head'
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+
+    const script_data = await db
+    .collection("script_data")
+    .find({"innerText": req.body})
+    // .sort({ metacritic: -1 })
+    // .limit(1)
+    .toArray();
+  res.json(script_data);
+};
